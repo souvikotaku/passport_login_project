8afa21c49b9e9bee60c63f90ba2dcf023f161cec
abc <abc@example.com>
2021-02-27 13:03:37 +0530

    okkk

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+
+//dotenv
+require('dotenv').config();
+
+
+//middlewares
+app.use('/uploads',express.static('uploads'));
+app.use(cors());
+app.use(express.json());
+
+// mongo uri important boilerplate
+//ATLAS_URI is the uri that i got from my mongo atlas, written on the env file
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, function(){
+  console.log('server has started')
+})
+
